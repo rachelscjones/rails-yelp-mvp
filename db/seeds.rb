ddbc31c539494c8ba1a27a9f1d90d7123ae8a0f4
Rachel Jones <rachelscjones@gmail.com>
2023-02-22 12:58:55 +0000

    Created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts 'generating seed file'
+Restaurant.destroy_all
+puts 'destroyed restaurants'
+
+category = %w[chinese italian japanese french belgian]
+
+8.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: category.sample
+  )
+end
